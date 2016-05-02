ac83bca510bc82f641d9bc4298eafb8e38da0219
BiaoDa <2656047991@qq.com>
2016-05-02 18:13:48 +0800

    '第三次提交'

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test{
 	public static void main(String[] args){
-		System.out.println("hello");
-		System.out.println("world");
+		int a =3;
+		int b =4;
+		System.out.println(a+b);
 	}
 }
